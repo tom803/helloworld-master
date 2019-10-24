58345a408c5e12456da255ca6a16885bc449d7cf
tom803 <47455535+tom803@users.noreply.github.com>
2019-10-24 23:15:29 +0200

    helloworld-web: Vorbereitung zur Auslieferung als war
    pom.xml:
    - packaging->war
    - properties-> startclass
    
    Application.java:
    - von SpringBootServletInitializer
    - configure übeschrieben

--- helloworld/src/main/java/de/springbootbuch/helloworld/Application.java
+++ helloworld/src/main/java/de/springbootbuch/helloworld/Application.java
@@ -2,12 +2,14 @@ package de.springbootbuch.helloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer{
 
 	@RestController
 	public static class HelloWorldController {
@@ -17,6 +19,11 @@ public class Application {
 			@RequestParam final String name) {
 			return "Hello, " + name + "\n";
 		}
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
 	}
 
 	public static void main(String... args) {
